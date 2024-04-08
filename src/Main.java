f3faa77834414aea8dcc1cb229b2f926422e4592
ArmBro24 <armbro24@gmail.com>
2024-04-08 21:17:10 +0500

    refactored a bit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.Objects;
+import java.util.SortedMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,35 +16,43 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         boolean menu = true;
 
-        while (menu) {
-            System.out.println("\n===ATM===");
-            System.out.println("\nEnter 'log' to login");
-            System.out.println("Enter 'reg' to register");
-            System.out.println("Enter 'stop' to stop program");
-            String m = sc.next();
+        try {
+            while (menu) {
+                System.out.println("\n===ATM===");
+                System.out.println("\nEnter 'log' to login");
+                System.out.println("Enter 'reg' to register");
+                System.out.println("Enter 'stop' to stop program");
+                String m = sc.next();
 
-            switch (m) {
-                case "reg" -> {
+                switch (m) {
+                    case "reg" -> {
 
-                    registration(databases);
-                }
+                        registration(databases);
+                    }
 
-                case "log" -> {
+                    case "log" -> {
+/*
                     authorization(databases);
-                }
+*/
+                        refAuthorization(databases);
+                    }
 
-                case "stop" -> {
-                    System.exit(0);
-                }
+                    case "stop" -> {
+                        System.exit(0);
+                    }
 
-                case "test" -> {
-                    myMethod();
-                }
+                    case "test" -> {
+                        myMethod();
+                    }
 
-                default -> {
-                    System.out.println("Unknown command, try again!");
+                    default -> {
+                        System.out.println("\nUnknown command, try again!");
+                    }
                 }
             }
+        }
+        catch (Exception e){
+            System.out.println("\nError occured");
         }
     }
     static void registration(Database databases){
@@ -53,7 +62,141 @@ public class Main {
         System.out.println("Success registration!");
     }
 
-    static void authorization(Database databases){
+    static void refAuthorization(Database databases){
+        Scanner sc = new Scanner(System.in);
+        Database database = databases;
+
+        boolean auth = true;
+        boolean accountFound = false;
+
+        System.out.print("\nLogin into account by number: ");
+        String acNum = sc.next();
+        System.out.print("Enter PIN: ");
+        String pinCode = sc.next();
+
+        try {
+            for(BankAccountNumber db : database.allAccounts){
+                if((Objects.equals(db.getAccountNumber(), acNum)) && (Objects.equals(db.getPinCode(), pinCode))){
+                    auth = false;
+                    accountFound = true;
+
+                    if(db instanceof CityBankAccount){
+                        while(accountFound){
+                            System.out.println("\n===CityBank===");
+                            System.out.println("\nChoose operation:");
+                            System.out.println("\nPress [1] for cash withdraw");
+                            System.out.println("Press [2] to view balance");
+                            System.out.println("Press [3] to change pin");
+                            System.out.println("Press [4] to insert cash into account");
+                            System.out.println("Press [5] to view account data");
+                            System.out.println("Press [6] to exit");
+                            int n = sc.nextInt();
+
+                            switch (n){
+                                case 1 -> {
+                                    System.out.print("Withdrawal amount: ");
+                                    int nummins = sc.nextInt();
+                                    db.creditBalance(nummins);
+                                    System.out.println("You have: " + db.totalBalance());
+                                    System.out.println();
+                                    break;
+                                }
+                                case 2 -> {
+                                    System.out.println("You have: " + db.totalBalance());
+                                    System.out.println();
+                                    break;
+                                }
+                                case 3 -> {
+                                    System.out.print("Enter new pin: ");
+                                    String newPin = sc.next();
+                                    db.setPinCode(newPin);
+                                    System.out.println("Pin successfully changed");
+                                    System.out.println();
+                                    break;
+                                }
+                                case 4 -> {
+                                    System.out.print("Enter amount to add: ");
+                                    int plus = sc.nextInt();
+                                    db.debetBalance(plus);
+                                    System.out.println("Your current balance: " + db.totalBalance());
+                                    System.out.println();
+                                    break;
+                                }
+                                case 5 -> {
+                                    System.out.println("CityBank account info");
+                                    System.out.println(db.accountData());
+                                    break;
+                                }
+                                case 6 -> {
+                                    accountFound = false;
+                                    auth = true;
+                                }
+                                default -> {
+                                    System.out.println("\nUnknown key, try again!");
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else if(db instanceof NationalBankAccount){
+                        while (accountFound){
+                            System.out.println("\n===NationalBank===");
+                            System.out.println("\nChoose operation:");
+                            System.out.println("\nPress [1] for cash withdraw");
+                            System.out.println("Press [2] to view balance");
+                            System.out.println("Press [3] to exit");
+                            int n = sc.nextInt();
+
+                            switch (n){
+                                case 1 -> {
+                                    System.out.print("Enter amount to withdraw: ");
+                                    int nummins = sc.nextInt();
+                                    db.creditBalance(nummins);
+                                    System.out.println("You have:" + db.totalBalance());
+                                    System.out.println();
+                                    break;
+                                }
+
+                                case 2 -> {
+                                    System.out.print("Your balance is: " + db.totalBalance());
+                                    break;
+                                }
+
+                                case 3 -> {
+                                    accountFound = false;
+                                    auth = true;
+                                }
+
+                                default -> {
+                                    System.out.println("\nUnknown key, try again!");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (!accountFound) {
+                System.out.println("\nAccount info not found!");
+            }
+        }
+        catch (Exception e){
+            System.out.println("\nError occured");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*static void authorization(Database databases){
         Scanner sc = new Scanner(System.in);
         Database database = databases;
 
@@ -177,7 +320,7 @@ public class Main {
                 break;
             }
         }
-    }
+    }*/
 
     static void myMethod(){
         System.out.println("Method separation");
